b27ab807a72dcf01fe60030fde76fb88e1d392e4
Лапенков Владимир <vlapenkov@ya.ru>
2018-06-20 14:09:43 +0300

    mat design added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatToolbarModule, MatInputModule, MatFormFieldModule, MatCardModule} from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule, 
+    MatInputModule, 
+    MatFormFieldModule, 
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
